0ad3770501237a8d05f84d1882f664e47b5c3294
Mira <miralilleholtvik@gmail.com>
2018-04-25 11:03:06 +0200

    lkjlkj

---
+++ main.m
@@ -0,0 +1,23 @@
+%% Project 2: Constrained Optimization
+
+
+%construct test problem
+
+
+%solve by using the 4 different methods
+
+
+
+%Barrier
+
+
+%SQP
+
+%Projected gradient
+
+%Augmented Lagrange
+
+%Plotting
+
+
+%Analyze what happends when lambdamin -> 0
