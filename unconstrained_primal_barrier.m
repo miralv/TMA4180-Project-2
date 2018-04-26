8dbbfac751aa7490fccc2f8803e33a9017431216
hakver29 <haakongv@stud.ntnu.no>
2018-04-26 14:29:53 +0200

    endret litt i henhold til pseudokoden, ikke kjørbart enda

--- unconstrained_primal_barrier.m
+++ unconstrained_primal_barrier.m
@@ -1,4 +1,11 @@
-function [x] = unconstrained_primal_barrier(x0,epsilon, z, w)
-    
+function [x] = unconstrained_primal_barrier(x0, z, w)
+    tau = 1;
+    mu = 1;
+    x = BFGS(x0,tau,z,w);
+    for i = 1:10
+        tau = tau/2;
+        mu = mu/2;
+        x = BFGS(x,tau,z,w);
+    end
 end
 
