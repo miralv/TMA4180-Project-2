8bfff7d835f20c522fcc796e4f69996a4e37a6f9
hakver29 <haakongv@stud.ntnu.no>
2018-04-30 09:22:17 +0200

    modified, works

--- eval_Pgrad.m
+++ eval_Pgrad.m
@@ -1,5 +1,5 @@
-function [P_grad] = eval_Pgrad(mu,x,z,w,n)
-
+function [P_grad] = eval_Pgrad(mu,x,z,w,lambda_minbound, lambda_maxbound)
+n = 2;
 [~,grad_f] = model_2eval(x,z,w,n);
 grad_c = mu*c_grad(x, lambda_minbound, lambda_maxbound);
 P_grad = grad_f + grad_c;
