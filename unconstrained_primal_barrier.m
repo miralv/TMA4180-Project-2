6f8feb8904479af488ea2eaa06d25b858264eeeb
hakver29 <haakongv@stud.ntnu.no>
2018-04-30 09:50:40 +0200

    add

--- unconstrained_primal_barrier.m
+++ unconstrained_primal_barrier.m
@@ -1,6 +1,7 @@
 % Hovedstrukturen til unconstrained primal barrier-metoden
 % Status: Ikke kjørbart. BFGS må modifiseres og input må endres i henhold
 % til unconstrained primal barrier.
+% Algorithm 19.5, page 584
 
 function [x] = unconstrained_primal_barrier(x0, z, w)
     tau = 1;
@@ -12,4 +13,3 @@ function [x] = unconstrained_primal_barrier(x0, z, w)
         x = BFGS(x,tau,z,w);
     end
 end
-
