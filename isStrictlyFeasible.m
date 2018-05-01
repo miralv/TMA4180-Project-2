40023b365cdc56434fdf706d6b0186d444f01363
Julie Wilberg <juliemwi@stud.ntnu.no>
2018-05-01 18:39:52 +0200

    strictly feasible

--- isStrictlyFeasible.m
+++ isStrictlyFeasible.m
@@ -1,5 +1,4 @@
 function res = isStrictlyFeasible(x,lambda_minbound,lambda_maxbound) %should take A has a parameter instead of building A every time
 % return 1 if x is in the feasible region omega, 0 if not 
-[A,~] = build_A_vec(x,2);
-res = (A(1,1)-lambda_minbound>0) && (-A(1,1)+ lambda_maxbound >0) && (A(2,2)- lambda_minbound >0) && (-A(2,2) + lambda_maxbound >0) && (sqrt(A(1,1)*A(2,2) - sqrt(lambda_minbound^2 + A(1,2)^2))> 0);
+res = (x(1)-lambda_minbound>0) && (-x(1)+ lambda_maxbound >0) && (x(2)- lambda_minbound >0) && (-x(2) + lambda_maxbound >0) && (sqrt(x(1)*x(2)) - sqrt(lambda_minbound^2 + x(3)^2))> 0;
 end
