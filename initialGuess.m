0306d001a69cf0868fc93d60cab89146a21c8b3b
hakver29 <haakongv@stud.ntnu.no>
2018-05-01 14:21:40 +0200

    add initialGuess

---
+++ initialGuess.m
@@ -0,0 +1,18 @@
+function [A,b] = initialGuess(lambda_minbound,lambda_maxbound)
+    A = eye(2);
+    b = zeros(2,1);
+    
+    A(1,1) = (lambda_maxbound-lambda_minbound)*rand() + lambda_minbound;
+    A(2,2) = (lambda_maxbound-lambda_minbound)*rand() + lambda_minbound;
+    
+    b(1) = (lambda_maxbound-lambda_minbound)*rand() + lambda_minbound;
+    b(2) = (lambda_maxbound-lambda_minbound)*rand() + lambda_minbound;
+    
+    A(1,2) = (lambda_maxbound-lambda_minbound)*rand() + lambda_minbound;
+    A(2,1) = A(1,2);
+    while det(A) < lambda_minbound^2
+       A(1,2) = (lambda_maxbound-lambda_minbound)*rand() + lambda_minbound;
+       A(2,1) = A(1,2);
+    end
+end
+
