248de9ec4ad06f248135f8b49efe8ba1b5610599
Sivert Selnes <sivertselnes@dhcp-10-22-35-230.wlan.ntnu.no>
2018-05-02 11:08:15 +0200

    clean

--- linesearch.m
+++ linesearch.m
@@ -4,40 +4,42 @@ function [alpha] = linesearch(x,p,Z,w,lambda_minbound,lambda_maxbound,mu)
     alpha = 1; 
     c1 = 1e-4;
     rho = 1/2;
-    
-    % Compute function ang gradient evaluation
+    not_finished = 1;
+    n_iter = 0;
+
+    % Compute function value and gradient
     f = eval_P(x,Z,w,mu,lambda_minbound,lambda_maxbound);
     grad = eval_Pgrad(mu,x,Z,w,lambda_minbound, lambda_maxbound);
     
-    % If we already have a feasible x + alpha*p, compute f_new
+    % If we already have a feasible x + alpha*p, compute a new f
     if isStrictlyFeasible(x+ alpha*p,lambda_minbound,lambda_maxbound)
         f_new = eval_P(x + alpha*p,Z,w,mu,lambda_minbound,lambda_maxbound);
     end
 
-    not_finished = 1;
-    n_iter = 0;
-
     while not_finished
+        
         if (~isStrictlyFeasible(x+ alpha*p,lambda_minbound,lambda_maxbound))
-            % Ensures feasibility
             alpha = alpha*rho;
             f_new = eval_P(x + alpha*p,Z,w,mu,lambda_minbound,lambda_maxbound);
             n_iter = n_iter + 1;
-        elseif (f_new > f + c1*alpha*grad.'*p)&& n_iter<50
-            % Tries to fulfill the sufficient decrease condition
+        
+         % Tries to fulfill the sufficient decrease condition
+        elseif (f_new > f + c1*alpha*grad.'*p) && n_iter<50    
             alpha = alpha*rho;
             f_new = eval_P(x + alpha*p,Z,w,mu,lambda_minbound,lambda_maxbound);
             n_iter = n_iter + 1;
+        
         else
-            if f_new <= f && isStrictlyFeasible(x+ alpha*p,lambda_minbound,lambda_maxbound)
-                not_finished = 0;
-            else
-                %error('Loop is stuck')
-                if ~ isStrictlyFeasible(x+ alpha*p,lambda_minbound,lambda_maxbound)
+            not_finished = 0;
+            
+            if ~ isStrictlyFeasible(x+ alpha*p,lambda_minbound,lambda_maxbound)
                 alpha = -1;
-                end
-                not_finished = 0;
             end
         end % if
     end % while
 end % function
+
+
+
+
+
