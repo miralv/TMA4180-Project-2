d985ef4bbed939dbbb5ffe239a46e35c3f8cc66e
Sivert Selnes <sivertselnes@dhcp-10-22-35-230.wlan.ntnu.no>
2018-05-02 10:18:08 +0200

    clean

--- main.m
+++ main.m
@@ -1,11 +1,8 @@
 close all
 clear
 
-%rng(20)
-%rng(19); % set seed random number generator
-error = 0; % 0 or 1 
+%rng(19); % set seed random number generator 
 epsilon = 1e-5;
-epsilon2 = 1e-8;
 dim = 2; % matrix dimension
 m = 50; % no. of z in test problem
 
@@ -17,11 +14,20 @@ mu = 1;
 max_iter = 200;
 
 % Making testproblems
-[z,w,A_true,vec_true] = testproblems(m,dim,error,modelnumber);
+[z,w,A_true,vec_true] = testproblems(m,dim,modelnumber);
 [A_init, b_init] = initialGuess(lambda_minbound, lambda_maxbound);
 
 % Converting to single x
 x0 = convert_from_A(A_init,b_init);
+
+% Solve optimization problem
+x = unconstrained_primal_barrier(x0, z, w,lambda_minbound,lambda_maxbound, epsilon,max_iter);
+
+% Reconstruct A and b/c 
+[A,vec] = build_A_vec(x,dim);
+
+% Plot and display error
+visualization(z,w,A,vec,A_true,vec_true);
 
 % % Plot points
 % [z1_pos,z1_neg,z2_pos,z2_neg] = find_points(z,w);
@@ -29,13 +35,3 @@ x0 = convert_from_A(A_init,b_init);
 % plot(z1_pos,z2_pos,'r+');
 % hold on
 % plot(z1_neg,z2_neg,'go');
-
-
-% Solve optimization problem
-x = unconstrained_primal_barrier(x0, z, w,lambda_minbound,lambda_maxbound, epsilon2,max_iter);
-
-% Reconstruct A and b/c 
-[A,vec] = build_A_vec(x,dim);
-eig(A)
-% Plot and display error
-visualization(z,w,A,vec,A_true,vec_true);
